d1cebc9b6370221adcad008d7d34aa873caa3aae
chenfu <chenfu@yuwo.com>
2019-08-26 09:56:38 +0800

    修改gitdemo

--- gitdemo/demospring/src/main/java/com/hunag/demospring/controller/GitDemo.java
+++ gitdemo/demospring/src/main/java/com/hunag/demospring/controller/GitDemo.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitDemo {
-    @GetMapping("/name")
+    @GetMapping("/good")
     public  String  gitDemo(){
         return "你是谁";
     }
