3e969ec85117a128c991eb785ffbcb9613e203ab
chenfu <chenfu@yuwo.com>
2019-08-26 09:59:36 +0800

    再次修改

--- gitdemo/demospring/src/main/java/com/hunag/demospring/controller/GitDemo.java
+++ gitdemo/demospring/src/main/java/com/hunag/demospring/controller/GitDemo.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GitDemo {
-    @GetMapping("/good")
+    @GetMapping("/test")
     public  String  gitDemo(){
         return "你是谁";
     }
